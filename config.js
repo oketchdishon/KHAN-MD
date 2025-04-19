7dff4bb216aeff7f89de20d186d07dec05fa9f79
oketchdishon <161639764+oketchdishon@users.noreply.github.com>
2025-04-19 12:12:23 +0300

    .

--- config.js
+++ config.js
@@ -65,7 +65,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923427582273",
+DEV: process.env.DEV || "254700282181",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
